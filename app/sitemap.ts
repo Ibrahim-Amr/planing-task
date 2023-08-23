1de26c84c03010a320af904d133c989b287c3649
Ibrahim Amr <ebrahimamra69@gmail.com>
2023-08-23 18:23:49 +0300

    fix:product insted of data

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     ...data.map((product: ProductType) => ({
-      url: `https://planing-task.vercel.app/product/${data.id}`,
+      url: `https://planing-task.vercel.app/product/${product.id}`,
       lastModified: new Date(),
     })),
   ];
