c9faf109709b3f4f23553db176947a83353f0753
Ibrahim Amr <ebrahimamra69@gmail.com>
2023-08-23 18:46:57 +0300

    sitemap link

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-	return {
-		rules: {
-			userAgent: '*',
-			allow: '/',
-			disallow: '/private/',
-		},
-		sitemap: 'https://ibrahimomar.vercel.app/sitemap.xml',
-	};
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://planing-task.vercel.app/sitemap.xml",
+  };
 }
